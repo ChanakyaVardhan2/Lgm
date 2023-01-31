2bc25d4c711f1d44a94deb8c9714a3082ef92349
SunWeb07 <sunnymawani786@gmail.com>
2023-01-31 11:04:07 +0530

    9211

---
+++ Task-3-Display_Form_Data/app.js
@@ -0,0 +1,92 @@
+window.onload = function(){
+let ArrayValue =[{fname:"Chanakya Vardhan",mail:"chanakya@mail.com",web_link:"www.chan.com",img_link:"user.png",gender:"male",checkval:["C","C++","JAVA"],}];
+
+if(localStorage.getItem('ObjValue')==null){
+    localStorage.setItem('ObjValue',JSON.stringify(ArrayValue));
+}
+}
+
+
+GetData=(e)=>{
+e.preventDefault();
+let Value = {fname:"",mail:"",web_link:"",img_link:"",gender:"",checkval:[],};
+ Value.fname = document.getElementById('name').value;
+ Value.mail = document.getElementById('email').value;
+ Value.web_link = document.getElementById('web-link').value;
+ Value.img_link = document.getElementById('img-link').value;
+
+
+
+
+if(document.getElementById('male').checked) {
+    Value.gender = "male";
+}
+else if(document.getElementById('female').checked) {
+    Value.gender="female";
+}
+else {
+    Value.gender="";
+}
+
+    var markedCheckbox = document.getElementsByName('skills');
+ 
+    for (var checkbox of markedCheckbox) {  
+      if (checkbox.checked)  
+      Value.checkval.push(checkbox.value);  
+    }  
+
+let ArrayValue = JSON.parse(localStorage.getItem('ObjValue'));
+ArrayValue.push(Value);
+localStorage.setItem("ObjValue",JSON.stringify(ArrayValue));
+
+  displayData();
+document.getElementById('name').value = "";
+document.getElementById('email').value = "";
+document.getElementById('web-link').value = "";
+document.getElementById('img-link').value = "";
+document.getElementById('male').checked = false;
+document.getElementById('female').checked = false;
+for(var checkbox of markedCheckbox){
+    if(checkbox.checked){
+        checkbox.checked = false;
+    }
+}
+  
+}
+document.getElementById('clear').addEventListener('click',()=>{
+    document.getElementById('form').reset();
+})
+
+
+displayData = (ArrayValue = undefined)=>{
+    let tableData ="";
+    let Value;
+    if(ArrayValue==undefined){
+        Value = JSON.parse(localStorage.getItem('ObjValue'));
+
+    }
+    else Value = ArrayValue;
+    Value.forEach(function(val){
+        let currentRow = `
+        <tr class="values" id="value">
+        <td>
+            <ul>
+                <li>
+                    <h3>Name: ${val.fname}</h3>
+                </li>
+                <li>Gender: ${val.gender}</li>
+                <li>Mail: <a href=${val.mail}>${val.mail}</a></li>
+                <li>Website: <a href=${val.web_link}>${val.web_link}</a> </li>
+                <li>Skills: ${val.checkval}</li> 
+            </ul>
+        </td>
+        <td>
+
+             <img src=${val.img_link} /> 
+        </td>
+    </tr>`
+    tableData+=currentRow;
+    });
+    document.getElementById('display').innerHTML = tableData; 
+}
+displayData();
